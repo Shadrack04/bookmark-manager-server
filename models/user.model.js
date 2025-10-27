49098f1bbee910d85e10a07678728c4ad1c4537a
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-10-28 00:25:05 +0100

    create user model

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+export const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User Name is required"],
+      trim: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    email: {
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/,
+        "Please enter a valid email address",
+      ],
+    },
+    password: {
+      required: true,
+      minLength: 6,
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
